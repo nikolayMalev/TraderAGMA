914462ee0d067ce513becd591e91a040782aacde
nikolayMalev <nikimalev98@gmail.com>
2024-06-15 21:08:30 +0300

    Style header

--- wp-content/themes/customTheme/templates/home.php
+++ wp-content/themes/customTheme/templates/home.php
@@ -12,50 +12,52 @@
     <header class="header">
         <div class="shell">
             <div class="header__inner">
-                <a href="#" class="logo">
-                    <span></span>
+                <div class="header__nav">
+                    <a href="#" class="logo">
+                        <span>Tradelogo</span>
 
-                    <p></p>
-                </a><!-- /.logo -->
+                        <p>Business Tagline</p>
+                    </a><!-- /.logo -->
 
-                <nav class="nav">
-                    <ul>
-                        <li>
-                            <a href="#">Home</a>
-                        </li>
+                    <nav class="nav">
+                        <ul>
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
 
-                        <li>
-                            <a href="#">How it Works</a>
-                        </li>
+                            <li>
+                                <a href="#">How it Works</a>
+                            </li>
 
-                        <li>
-                            <a href="#">Programs</a>
-                        </li>
+                            <li>
+                                <a href="#">Programs</a>
+                            </li>
 
-                        <li>
-                            <a href="#">Symbols</a>
-                        </li>
+                            <li>
+                                <a href="#">Symbols</a>
+                            </li>
 
-                        <li>
-                            <a href="#">Learn</a>
-                        </li>
+                            <li>
+                                <a href="#">Learn</a>
+                            </li>
 
-                        <li>
-                            <a href="#">Competition</a>
-                        </li>
+                            <li>
+                                <a href="#">Competition</a>
+                            </li>
 
-                        <li>
-                            <a href="#">Be a Partner</a>
-                        </li>
+                            <li>
+                                <a href="#">Be a Partner</a>
+                            </li>
 
-                        <li>
-                            <a href="#">About Us</a>
-                        </li>
-                    </ul>
-                </nav><!-- /.nav -->
+                            <li>
+                                <a href="#">About Us</a>
+                            </li>
+                        </ul>
+                    </nav><!-- /.nav -->
+                </div><!-- /.header__nav -->
 
                 <div class="header__actions">
-                    <a href="" class="btn">Dashboard</a>
+                    <a href="" class="header__btn">Dashboard</a>
                 </div><!-- /.header__actions -->
             </div><!-- /.header__inner -->
         </div><!-- /.shell --> 
